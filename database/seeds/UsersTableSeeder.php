e8821a1ed2e7f248306de048bce54eb3e250959a
OstapHetman <prickly4528@gmail.com>
2019-06-20 12:08:37 +0300

    seeding data

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,5 +18,12 @@ class UsersTableSeeder extends Seeder
             'avatar' => asset('avatars/avatar.png'),
             'admin' => 1
         ]);
+
+        App\User::create([
+            'name' => 'Test',
+            'password' => bcrypt('admin'),
+            'email' => "test@test.com",
+            'avatar' => asset('avatars/avatar.png')
+        ]);
     }
 }
