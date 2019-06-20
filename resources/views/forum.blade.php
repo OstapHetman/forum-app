4260b239161392986f6762378381d3d89bb50290
OstapHetman <prickly4528@gmail.com>
2019-06-20 13:18:00 +0300

    discussion card style

--- resources/views/forum.blade.php
+++ resources/views/forum.blade.php
@@ -6,11 +6,22 @@
     @foreach ($discussions as $d)
         <div class="card mb-4">
             <div class="card-header">
-                <img src="{{ $d->user->avatar }}" alt="" width="70px" height="70px">
+                <img src="{{ $d->user->avatar }}" alt="" width="40px" height="40px">&nbsp;&nbsp;&nbsp;
+                <span>{{ $d->user->name }}</span>
+                <a href="{{ route('discussion', ['slug' => $d->slug]) }}" class="btn btn-secondary float-right">View</a>
             </div>
     
             <div class="card-body">
-                {{ $d->content }}
+                <h4 class="text-center">
+                    {{ $d->title }}
+                </h4>
+                <p class="text-center">
+                    {{ str_limit($d->content, 50) }}
+                </p>
+            </div>
+
+            <div class="card-footer">
+                <p class="mb-0">{{ $d->replies->count() }} Replies</p>
             </div>
         </div>
     @endforeach
